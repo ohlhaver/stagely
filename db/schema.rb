cc27703bdd380b29fe3943368ce536461a61d9f5
Justus Ohlhaver <ohlhaver@gmail.com>
2012-03-14 23:51:51 +0100

    clean up tag table mess

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120312224326) do
+ActiveRecord::Schema.define(:version => 20120314224923) do
 
   create_table "events", :force => true do |t|
     t.text     "description"
