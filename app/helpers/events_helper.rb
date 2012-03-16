7ff7a9627d159efdc3c82f0e8874f9dec388b17f
Justus Ohlhaver <ohlhaver@gmail.com>
2012-03-16 21:03:15 +0100

    link to map and increase font

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,9 +1,6 @@
 # -*- encoding : utf-8 -*-
 module EventsHelper
 
-	def complete_address(e)
-		return e.venue.name + " - " + e.venue.street
-	end
 
 	def add_new_venue
 		return link_to 'add new venue', new_venue_path
