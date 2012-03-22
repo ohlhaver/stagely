1769d7a953c8aae7edad09151ea7c8a6975e1352
Justus Ohlhaver <ohlhaver@gmail.com>
2012-03-22 01:59:16 +0100

    fix omni auth environments

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,16 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '367315919957325','1eb6002c20b12b3004261fb36dd5db0e', {:scope => ""}
+if Rails.env.production?
+   	
+   	Rails.application.config.middleware.use OmniAuth::Builder do
+  		provider :facebook, '367315919957325','1eb6002c20b12b3004261fb36dd5db0e', {:scope => ""}
+	end
 
+elsif Rails.env.development?
+   	
+   	Rails.application.config.middleware.use OmniAuth::Builder do
+  		provider :facebook, '330051837042287','c12a7d79dbe85e2582cea2fee6c51256', {:scope => ""}
+  	end
+else  
+	Rails.application.config.middleware.use OmniAuth::Builder do
+  		provider :facebook, '367315919957325','1eb6002c20b12b3004261fb36dd5db0e', {:scope => ""}
+  	end
 end
